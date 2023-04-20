a827ae3e07ce78a3da2f6679f7a9b3fff8f5d174
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-04-20 18:16:04 -0300

    criação das tabelas

--- backend-hubla/src/data/queries.sql
+++ backend-hubla/src/data/queries.sql
@@ -1,2 +1,21 @@
 
 
+
+
+CREATE TABLE sellers (
+    id TEXT UNIQUE NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL, --affiliate / creator
+    balance REAL NOT NULL
+);
+
+CREATE TABLE transactions (
+    id TEXT UNIQUE NOT NULL PRIMARY KEY,
+    type INT NOT NULL,
+    date TEXT NOT NULL,
+    product TEXT NOT NULL,
+    value REAL NOT NULL,
+    seller_id TEXT NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES sellers(id)
+);
+
